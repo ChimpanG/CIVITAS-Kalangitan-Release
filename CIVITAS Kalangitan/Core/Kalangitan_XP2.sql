ff1145e38f0157e0afe97fa21070500dd5f0a7a6
ChimpanG <chimpang.mod@gmail.com>
2019-07-22 02:15:32 +0100

    Added vanilla compatibility

---
+++ CIVITAS Kalangitan/Core/Kalangitan_XP2.sql
@@ -0,0 +1,13 @@
+/*
+	XP2
+	Authors: ChimpanG
+*/
+
+-----------------------------------------------
+-- Improvement_ValidFeatures
+-----------------------------------------------
+
+INSERT INTO Improvement_ValidFeatures
+		(ImprovementType,					FeatureType,						PrereqCivic	)
+VALUES	('IMPROVEMENT_CVS_KALANGITAN_UI',	'FEATURE_FLOODPLAINS_GRASSLAND',	NULL		),
+		('IMPROVEMENT_CVS_KALANGITAN_UI',	'FEATURE_FLOODPLAINS_PLAINS',		NULL		);
