05a8a2743e7e8f25d0751c8cf333beba80d14349
ChimpanG <chimpang.mod@gmail.com>
2019-11-01 12:30:21 +0000

    Minor fixes

--- CIVITAS Kalangitan/Core/Kalangitan_XP2.sql
+++ CIVITAS Kalangitan/Core/Kalangitan_XP2.sql
@@ -8,6 +8,6 @@
 -----------------------------------------------
 
 INSERT INTO Improvement_ValidFeatures
-		(ImprovementType,					FeatureType,						PrereqCivic	)
-VALUES	('IMPROVEMENT_CVS_KALANGITAN_UI',	'FEATURE_FLOODPLAINS_GRASSLAND',	NULL		),
-		('IMPROVEMENT_CVS_KALANGITAN_UI',	'FEATURE_FLOODPLAINS_PLAINS',		NULL		);
+		(ImprovementType,					FeatureType						)
+VALUES	('IMPROVEMENT_CVS_KALANGITAN_UI',	'FEATURE_FLOODPLAINS_GRASSLAND'	),
+		('IMPROVEMENT_CVS_KALANGITAN_UI',	'FEATURE_FLOODPLAINS_PLAINS'	);
